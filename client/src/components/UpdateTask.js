92224cd27fc146904bea4caafc70a77d7bddf161
Jerry Nkiambuo <nkiambuojerry98@gmail.com>
2024-05-14 16:25:41 +0100

    created the updatetask file and built code in it.

---
+++ client/src/components/UpdateTask.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useParams, useHistory } from 'react-router-dom';
+
+const UpdateTask = () => {
+  const { id } = useParams();
+  const [name, setName] = useState('');
+  const history = useHistory();
+
+  useEffect(() => {
+    axios.get(`/api/tasks/${id}`)
+      .then(response => setName(response.data.name))
+      .catch(error => console.error(error));
+  }, [id]);
+
+  const updateTask = () => {
+    axios.put(`/api/tasks/${id}`, { name })
+      .then(() => history.push('/'))
+      .catch(error => console.error(error));
+  };
+
+  return (
+    <div>
+      <h1>Update Task</h1>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <button onClick={updateTask}>Update</button>
+    </div>
+  );
+};
+
+export default UpdateTask;
