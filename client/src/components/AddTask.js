dbe806884ff48ee011c45ec23cc81fee87db6e5a
Jerry Nkiambuo <nkiambuojerry98@gmail.com>
2024-05-15 12:03:34 +0100

    added import statements

--- client/src/components/AddTask.js
+++ client/src/components/AddTask.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { useHistory } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 const AddTask = () => {
   const [name, setName] = useState('');
--- client/src/components/UpdateTask.js
+++ client/src/components/UpdateTask.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { useParams, useHistory } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 const UpdateTask = () => {
   const { id } = useParams();
