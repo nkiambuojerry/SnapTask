fa1d8926b4e109e9025aab3cda8eba3b75f7224e
Jerry Nkiambuo <nkiambuojerry98@gmail.com>
2024-05-17 00:05:37 +0100

    Updated Server Folder

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const Task = require("./model/taskModel.js");
 const taskRoute = require("./routes/taskRoute.js");
 const app = express();
+require('dotenv').config();
 
 // middleware
 app.use(express.json());
@@ -20,15 +21,16 @@ app.get("/", (req, res) => {
   res.send("Hello from Node API Server Updated");
 });
 
+const PORT = process.env.PORT;
 
 mongoose
   .connect(
-    "mongodb+srv://basit1:basit123@cluster0.9voj9be.mongodb.net/"
+    process.env.MONGO_URL
   )
   .then(() => {
     console.log("Connected to database!");
-    app.listen(3001, () => {
-      console.log("Server is running on port 3000");
+    app.listen(PORT, () => {
+      console.log("Server is running on port", PORT);
     });
   })
   .catch(() => {
